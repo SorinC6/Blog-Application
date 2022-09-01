5e64b82e533d841377c05837966d88026781f325
SorinC6 <kish.sorin@yahoo.com>
2022-09-01 13:22:02 +0300

    add query for recent and similar post

--- services/index.js
+++ services/index.js
@@ -5,7 +5,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 export const getPosts = async () => {
     const query = gql`
         query MyQuery {
-            postsConnection {
+            postsConnection(orderBy: createdAt_DESC) {
             edges {
                 node {
                 authors {
@@ -33,6 +33,47 @@ export const getPosts = async () => {
         }
     `
 
+    const result = await request(graphqlAPI, query)
+    return result.postsConnection.edges
+}
+
+export const getRecentPosts = async () => {
+    const query = gql`
+        query getPostDetails(){
+            posts(
+                orderBy: createdAt_ASC
+                last: 3 
+                ){
+                    title
+                    featuredimage {
+                        url
+                    }
+                    createdAt
+                    slug
+                }
+            
+        }
+    `
+    const result = await request(graphqlAPI, query)
+    return result.postsConnection.edges
+}
+
+export const getSimmilarPosts = async () => {
+    const query = gql`
+        query getPostDetails($slug: String!, $categories: [String!]) {
+            posts(
+                where: {slug_not: $slug, AND: {categories_sum:{slub_in: $categories}}}
+                last: 3
+            ){
+                title
+                featuredimage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `
     const result = await request(graphqlAPI, query)
     return result.postsConnection.edges
 }
