4c55db94e58c8c0b70808dfba620ca52249286a1
SorinC6 <kish.sorin@yahoo.com>
2022-09-01 12:13:35 +0300

    add graphql query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,21 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  {
-    title: "React Test",
-    excerpt: "Learn React Testing",
-  },
-  {
-    title: "React CSS",
-    excerpt: "Learn Styled components",
-  },
-];
-const Home: NextPage = () => {
+// const posts = [
+//   {
+//     title: "React Test",
+//     excerpt: "Learn React Testing",
+//   },
+//   {
+//     title: "React CSS",
+//     excerpt: "Learn Styled components",
+//   },
+// ];
+
+const Home: NextPage<any> = ({ posts }) => {
+  console.log("posts", posts);
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -21,7 +24,7 @@ const Home: NextPage = () => {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => (
+          {posts.map((post: Object, index: number) => (
             <PostCard post={post} key={index} />
           ))}
         </div>
@@ -35,5 +38,13 @@ const Home: NextPage = () => {
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
+}
 
 export default Home;
