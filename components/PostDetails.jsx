ff8e617eed2e13c606322681a119c11e2669f5b5
SorinC6 <kish.sorin@yahoo.com>
2022-09-02 11:51:38 +0300

    add post details

--- components/PostDetails.jsx
+++ components/PostDetails.jsx
@@ -1,7 +1,156 @@
 import React from "react";
+import moment from "moment";
+import Link from "next/link";
+import { RichText } from "@graphcms/rich-text-react-renderer";
 
-const PostDetails = () => {
-  return <div>PostDetails</div>;
+const PostDetails = ({ post }) => {
+  console.log("post.content.raw", post.content.raw);
+
+  const getContentFragment = (index, text, obj, type) => {
+    let modifiedText = text;
+
+    if (obj) {
+      if (obj.bold) {
+        modifiedText = <b key={index}>{text}</b>;
+      }
+
+      if (obj.italic) {
+        modifiedText = <em key={index}>{text}</em>;
+      }
+
+      if (obj.underline) {
+        modifiedText = <u key={index}>{text}</u>;
+      }
+    }
+
+    switch (type) {
+      case "heading-three":
+        return (
+          <h3 key={index} className="text-xl font-semibold mb-4">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h3>
+        );
+      case "paragraph":
+        return (
+          <p key={index} className="mb-8">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </p>
+        );
+      case "heading-four":
+        return (
+          <h4 key={index} className="text-md font-semibold mb-4">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h4>
+        );
+      case "bulleted-list":
+        console.log("bulleted-list", modifiedText);
+        return (
+          <p key={index} className="text-md mb-4" style={{ color: "red" }}>
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </p>
+        );
+      case "code-block":
+        return (
+          <pre
+            className="whitespace-pre-wrap"
+            style={{
+              backgroundColor: "#F8F9FB",
+              padding: "15px",
+              marginBottom: "10px",
+              fontSize: "12px",
+              wordBreak: "break-word",
+              overflowWrap: "break-all",
+            }}
+          >
+            <code
+              key={index}
+              className="mb-4 whitespace-pre-wrap"
+              style={{ wordBreak: "break-word", overflowWrap: "break-all" }}
+            >
+              {modifiedText.map((item, i) => (
+                <React.Fragment key={i}>{item}</React.Fragment>
+              ))}
+            </code>
+          </pre>
+        );
+      case "image":
+        return (
+          <img
+            key={index}
+            alt={obj.title}
+            height={obj.height}
+            width={obj.width}
+            src={obj.src}
+          />
+        );
+      default:
+        return modifiedText;
+    }
+  };
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
+      <div className="relative overflow-hidden shadow-md mb-6">
+        <img
+          src={post.featuredimage.url}
+          alt=""
+          className="object-top h-full w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg"
+        />
+      </div>
+      <div className="px-4 lg:px-0">
+        <div className="flex items-center mb-8 w-full">
+          <div className="hidden md:flex items-center justify-center lg:mb-0 lg:w-auto mr-8 items-center">
+            <img
+              alt={post.authors[0].name}
+              height="30px"
+              width="30px"
+              className="align-middle rounded-full"
+              src={post.authors[0].photo.url}
+            />
+            <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">
+              {post.authors[0].name}
+            </p>
+          </div>
+          <div className="font-medium text-gray-700">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 inline mr-2 text-pink-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+              />
+            </svg>
+            <span className="align-middle">
+              {moment(post.createdAt).format("MMM DD, YYYY")}
+            </span>
+          </div>
+        </div>
+        <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+        {post.content.raw.children.map((typeObj, index) => {
+          const children = typeObj.children.map((item, itemindex) =>
+            getContentFragment(itemindex, item.text, item)
+          );
+
+          return getContentFragment(index, children, typeObj, typeObj.type);
+        })}
+        {/* <RichText content={post.content.raw} /> */}
+      </div>
+    </div>
+  );
 };
 
 export default PostDetails;
