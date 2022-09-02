03bc515ff333e5a4a24b1fea988250aafa427885
SorinC6 <kish.sorin@yahoo.com>
2022-09-02 16:03:49 +0300

    added comment form and grapql mutation

---
+++ pages/api/comments.js
@@ -0,0 +1,34 @@
+import { GraphQLClient, gql } from 'graphql-request';
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+const graphqlToken = process.env.NEXT_PUBLIC_GRAPHQL_CMS_TOKEN
+
+/** *************************************************************
+* Any file inside the folder pages/api is mapped to /api/* and  *
+* will be treated as an API endpoint instead of a page.         *
+*************************************************************** */
+
+// export a default function for API route to work
+export default async function asynchandler(req, res) {
+    console.log("graphqlToken", { graphqlToken })
+    const graphQLClient = new GraphQLClient((graphqlAPI), {
+        headers: {
+            authorization: `Bearer ${graphqlToken}`,
+        },
+    });
+
+    const query = gql`
+    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
+      createComment(data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}) { id }
+    }
+  `;
+
+    const result = await graphQLClient.request(query, {
+        name: req.body.name,
+        email: req.body.email,
+        comment: req.body.comment,
+        slug: req.body.slug,
+    });
+
+    return res.status(200).send(result);
+}
