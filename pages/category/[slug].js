c53aa075c7d5dd075eca6866fe2d004ceeb2b75c
SorinC6 <kish.sorin@yahoo.com>
2022-09-04 19:26:57 +0300

    add extra check

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -16,7 +16,7 @@ const CategoryPost = ({ posts }) => {
         <div className="container mx-auto px-10 mb-8">
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                 <div className="col-span-1 lg:col-span-8">
-                    {posts.map((post, index) => (
+                    {posts?.map((post, index) => (
                         <PostCard key={index} post={post.node} />
                     ))}
                 </div>
