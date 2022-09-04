83955adef4f32f8da08d2a492b3c70ad3424f76f
SorinC6 <kish.sorin@yahoo.com>
2022-09-04 19:49:33 +0300

    wip

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -15,11 +15,15 @@ const ArticleDetails = ({ post }) => {
         <div className="container mx-auto px-10 mb-8">
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                 <div className="col-span-1 lg:col-span-8">
-                    {post && <PostDetails post={post} />}
-                    <Author author={post?.authors[0]} />
-                    <AdjacentPosts slug={post?.slug} createdAt={post?.createdAt} />
-                    <CommentsForm slug={post?.slug} />
-                    <Comments slug={post?.slug} />
+                    {post &&
+                        <>
+                            <PostDetails post={post} />
+                            <Author author={post?.authors[0]} />
+                            <AdjacentPosts slug={post?.slug} createdAt={post?.createdAt} />
+                            <CommentsForm slug={post?.slug} />
+                            <Comments slug={post?.slug} />
+                        </>
+                    }
                 </div>
                 <div className="col-span-1 lg:col-span-4">
                     <div className="relative lg:sticky top-8">
