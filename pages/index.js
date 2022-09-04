c11814e6e23fe7aad072adf492fef9000713e48d
SorinC6 <kish.sorin@yahoo.com>
2022-09-04 18:58:37 +0300

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,18 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
 import { getPosts } from "../services";
 import { FeaturedPosts } from "../sections/index";
 
-const Home: NextPage<any> = ({ posts }) => {
+const Home = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>Student Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post: Object, index: number) => (
+          {posts.map((post, index) => (
             <PostCard post={post?.node} key={index} />
           ))}
         </div>
