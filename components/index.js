ffc0e49a3d5637f5f8844c507aa0620e9f4bd6bf
SorinC6 <kish.sorin@yahoo.com>
2022-09-04 19:04:25 +0300

    add loader

--- components/index.js
+++ components/index.js
@@ -1,6 +1,7 @@
 export { default as PostCard } from './PostCard'
 export { default as Categories } from './Categories'
 export { default as PostWidget } from './PostWidget'
+export { default as Loader } from './Loader'
 export { default as Header } from './Header'
 export { default as Layout } from './Layout'
 export { default as PostDetails } from './PostDetails'
