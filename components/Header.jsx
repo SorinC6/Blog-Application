5ea2314ea7f544fabb3e2e10bb5b1a53962c6a83
SorinC6 <kish.sorin@yahoo.com>
2022-09-12 18:11:16 +0300

    wip

--- components/Header.jsx
+++ components/Header.jsx
@@ -42,9 +42,7 @@ const Header = () => {
     categories.map((category, index) => (
       <Link key={index} href={`/category/${category.slug}`}>
         <span
-          className={`md:float-${position} mt-2 align-middle text-white ${
-            position === "left" ? "mr" : "ml"
-          }-4 font-semibold cursor-pointer `}
+          className={`md:float-${position} mt-3  align-middle text-white mr-4 font-semibold cursor-pointer text-xs lg:text-sm`}
         >
           {category.name}
         </span>
@@ -52,7 +50,7 @@ const Header = () => {
     ));
 
   return (
-    <div className="container mx-auto px-5 mb-8">
+    <div className="container mx-auto px-5 mb-8 md:px-10">
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="flex flex-col">
           <div>
@@ -70,13 +68,11 @@ const Header = () => {
               />
             </div>
             {/* OLNY ON XL to show the category in a row - right side */}
-            <div className="hidden md:float-left xl:contents last:mr-0">
+            {/* <div className="hidden md:float-left xl:contents last:mr-0">
               {renderCategories("right")}
-            </div>
+            </div> */}
           </div>
-          <div className="hidden sm:block xl:hidden flex">
-            {renderCategories("left")}
-          </div>
+          <div className="flex w-full">{renderCategories("left")}</div>
         </div>
       </div>
     </div>
